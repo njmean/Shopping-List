18f054d03f0db2d29c85ae56b67200dab4855099
njmean <nigel.meaney@gmail.com>
2020-10-20 15:57:29 +0100

    Heroku

--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -11,7 +11,6 @@ import {
 } from 'reactstrap';
 import { connect } from 'react-redux';
 import { addItem } from '../actions/itemActions';
-import { v4 as uuidv4 } from 'uuid';
 
 class ItemModal extends Component {
     state = {
@@ -34,7 +33,6 @@ class ItemModal extends Component {
     onSubmit = (e) => {
         e.preventDefault();
         const newItem = {
-            id: uuidv4(),
             name: this.state.name
         }
 
--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,34 +1,38 @@
-import { startSession } from 'mongoose';
-import { v4 as uuidv4 } from 'uuid';
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM} from '../actions/types';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from '../actions/types';
 
 const initialState = {
 
-    items: [
-        { id: uuidv4(), name: "Eggs" },
-        { id: uuidv4(), name: "Milk" },
-        { id: uuidv4(), name: "Steak" },
-        { id: uuidv4(), name: "Water" }
-    ]
-}
+    items: [],
+    loading: false
+};
 
-export default function(state = initialState, action) {
-    switch(action.type) {
+export default function (state = initialState, action) {
+    switch (action.type) {
         case GET_ITEMS:
             return {
-                ...state
+                ...state,
+                items: action.payload,
+                loading: false
             };
-            case DELETE_ITEM:
-                return {
-                    ...state,
-                    items: state.items.filter(item => item.id !== action.payload)
-                };
+        case DELETE_ITEM:
+            return {
+                ...state,
+                items: state.items.filter(item => item._id !== action.payload)
+            };
 
-            case ADD_ITEM:
-                return {
-                    ...state,
-                    items: [action.payload, ...state.items]
-                };
+        case ADD_ITEM:
+            return {
+                ...state,
+                items: [action.payload, ...state.items]
+            };
+
+        case ITEMS_LOADING:
+            return {
+                ...state,
+                loading: true
+
+            };
+
         default:
             return state;
     }
